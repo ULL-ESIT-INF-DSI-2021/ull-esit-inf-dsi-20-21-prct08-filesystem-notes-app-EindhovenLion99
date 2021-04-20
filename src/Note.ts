a0939be5e29d5f66ac1d74a1ce373938b6eab581
Noah Sanchez <alu0101134956@ull.edu.es>
2021-04-20 12:22:31 +0100

    Añadiendo clase Note

---
+++ src/Note.ts
@@ -0,0 +1,54 @@
+import * as chalk from 'chalk';
+
+/**
+ * # Class Note
+ * Esta clase se encargara de la estructura y componentes de cada nota.
+ * 
+ * ## Atributos
+ * Los atributos de la clase Note son los siguientes:
+ * @param Title
+ * @param Body
+ * @param Color
+ * 
+ * ## Metodos
+ * Son los setters y getters basicos de la clase, para poder modificar y obtener los valores de la clase
+ * @method setTitle() Modifica el titulo de la nota
+ * @method setBody() Modifica el cuerpo de la nota
+ * @method setColor() Modifica el color de la nota
+ * 
+ * @method getTitle() Retorna el titulo de la nota
+ * @method getBody() Retorna el cuerpo de la nota
+ * @method getColor() Retorna el color de la nota
+ */
+
+export type TypeColor = "Red" | "Green" | "Blue" | "Yellow";
+
+export class Note {
+  constructor(private Title: string,
+              private Body: string,
+              private Color: TypeColor) {}
+
+  setName(newTitle: string): void {
+    this.Title = newTitle;
+  }
+
+  setBody(newBody: string): void {
+    this.Body = newBody;
+  }
+
+  setColor(newColor: TypeColor): void {
+    this.Color = newColor;
+  }
+
+  getTitle(): string {
+    return this.Title;
+  }
+
+  getBody(): string {
+    return this.Body;
+  }
+
+  getColor(): TypeColor {
+    return this.Color;
+  }
+}
